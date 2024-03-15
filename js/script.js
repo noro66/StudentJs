66683bed7405ed5db5eb295cf0d91c9b7af53cd6
Mogy <norou6@gmail.com>
2024-03-15 11:57:23 +0000

    add delte button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 import Student from "./student.js";
 
 const  displayStudents = function (){
-    Student.allStudents().then(function(response){
-        const tableBody = response.map((student) => {
-            console.log(student.id)
+   return  Student.allStudents().then(function(response){
+        return response.map((student) => {
+            const {id, name, age, point} = student
+            return `<tr>
+                       <td>${id}</td>
+                       <td>${name}</td>
+                       <td>${age}</td>
+                       <td>${point}</td>
+                       <td><button class="btn btn-danger btn-sm ">Delete</button></td>
+                   </tr>`;
         })
     })
 
 }
-displayStudents();
+displayStudents().then((response) => {
+    const  body = document.getElementById('list-student').innerHTML = response.join('');
+    // console.log(response.join(' '));
+});
--- js/student.js
+++ js/student.js
@@ -2,16 +2,13 @@ import {ENDPOINT} from "./endpoint.js";
 
 export default class Student {
 
-        construtor(name, age, note){
+        construtor(name, age, note, points){
             this.name = name
             this.age = age
-            this.note = note
+            this.points = points
     }
     static  allStudents = async function () {
        const response = await fetch(ENDPOINT)
         return await response.json();
-
     }
-
-
 }
