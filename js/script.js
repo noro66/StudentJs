bd42cb9c81fd2ae8e57b78be575e30bb9574111b
Mogy <norou6@gmail.com>
2024-03-15 14:25:48 +0000

    add student

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 import Student from "./student.js";
+const form = document.getElementById('form');
 
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const [name, birthday, point] = document.querySelectorAll('#name, #birthday, #point');
+    console.log(name.value, birthday.value, point.value)
+    const student = new Student(name.value, birthday.value, point.value);
+    console.log(student.addStudent());
+})
 const  displayStudents = function (){
    return  Student.allStudents().then(function(response){
         return response.map((student) => {
-            const {id, name, age, point} = student
+            const {id, name, birthday, point} = student
             return `<tr>
                        <td>${id}</td>
                        <td>${name}</td>
-                       <td>${age}</td>
+                       <td>${birthday}</td>
                        <td>${point}</td>
                        <td><button class="btn btn-danger btn-sm ">Delete</button></td>
                    </tr>`;
@@ -15,6 +23,7 @@ const  displayStudents = function (){
     })
 
 }
+
 displayStudents().then((response) => {
     const  body = document.getElementById('list-student').innerHTML = response.join('');
     // console.log(response.join(' '));
--- js/student.js
+++ js/student.js
@@ -2,13 +2,29 @@ import {ENDPOINT} from "./endpoint.js";
 
 export default class Student {
 
-        construtor(name, age, note, points){
+        constructor(name, birthday, point){
             this.name = name
-            this.age = age
-            this.points = points
+            this.birthday = birthday
+            this.point = point
     }
     static  allStudents = async function () {
        const response = await fetch(ENDPOINT)
         return await response.json();
+    }
+    response;
+
+    addStudent = async function(){
+            return  await fetch(ENDPOINT, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: this.name,
+                    birthday: this.birthday,
+                    point: this.point
+                })
+            }).then(response => response)
+                .catch(reportError => reportError);
     }
 }
